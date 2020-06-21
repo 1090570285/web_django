cd15cdbb7c9783277be8a62dfe09de5b0684f046
1090570285 <1090570285@qq.com>
2020-06-21 16:17:12 +0800

    字数统计的源代码

---
+++ web_django/function.py
@@ -0,0 +1,25 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    # return HttpResponse('李宝华')
+    return render(request, 'index.html')
+
+
+def result(request):
+    user_text = request.GET['text']
+    count_sum = len(user_text)
+    word_dict = {}
+    for word in user_text:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+    sorted_dict = sorted(word_dict.items(), key=lambda w: w[1], reverse=True)
+
+
+    return render(request, 'result.html',
+                  {'num_sum' : count_sum , 'text': user_text,
+                   'dict': word_dict,
+                   'sorted':sorted_dict})
